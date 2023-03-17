4e4229ba08fcfda6da8f5f07cf9b6cef9b55f726
Edin Jašarević <edinjasar14@gmail.com>
2023-03-17 21:47:34 +0100

    using ILogger<T> logging

--- CarvedRock.Api/Controllers/ProductsController.cs
+++ CarvedRock.Api/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using CarvedRock.Api.ApiModels;
 using CarvedRock.Api.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace CarvedRock.Api.Controllers
 {
@@ -9,16 +10,20 @@ namespace CarvedRock.Api.Controllers
     [Route("[controller]")]
     public class ProductsController : ControllerBase
     {
+        private readonly ILogger<ProductsController> _logger;
         private readonly IProductLogic _productLogic;
 
-        public ProductsController(IProductLogic productLogic)
+        public ProductsController(ILogger<ProductsController> logger, IProductLogic productLogic)
         {
+            _logger = logger;
             _productLogic = productLogic;
         }
 
         [HttpGet]
         public IEnumerable<Product> GetProducts(string category = "all")
         {
+            _logger.LogInformation("Starting controller action GetProduct for {category}", category);
+
             return _productLogic.GetProductsForCategory(category);
         }
     }
