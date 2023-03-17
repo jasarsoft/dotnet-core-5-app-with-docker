29ed875cd1a5591d4d9adb539c195d103062df3b
Edin Jašarević <edinjasar14@gmail.com>
2023-03-17 22:05:06 +0100

    Log ws ILogger

--- CarvedRock.Api/Controllers/ProductsController.cs
+++ CarvedRock.Api/Controllers/ProductsController.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Generic;
-using CarvedRock.Api.ApiModels;
+﻿using CarvedRock.Api.ApiModels;
 using CarvedRock.Api.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using System.Collections.Generic;
 
 namespace CarvedRock.Api.Controllers
 {
@@ -10,19 +10,17 @@ namespace CarvedRock.Api.Controllers
     [Route("[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly ILogger<ProductsController> _logger;
         private readonly IProductLogic _productLogic;
 
-        public ProductsController(ILogger<ProductsController> logger, IProductLogic productLogic)
+        public ProductsController(IProductLogic productLogic)
         {
-            _logger = logger;
             _productLogic = productLogic;
         }
 
         [HttpGet]
         public IEnumerable<Product> GetProducts(string category = "all")
         {
-            _logger.LogInformation("Starting controller action GetProduct for {category}", category);
+            Log.Information("Starting controller action GetProduct for {category}", category);
 
             return _productLogic.GetProductsForCategory(category);
         }
