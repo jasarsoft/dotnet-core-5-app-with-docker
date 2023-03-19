369568dd84c020cab603e979288301f1c44911e3
Edin Jašarević <edinjasar14@gmail.com>
2023-03-19 11:23:43 +0100

    enable configuration in dotnet console app

--- CarvedRock.InvoiceGenerator/Program.cs
+++ CarvedRock.InvoiceGenerator/Program.cs
@@ -1,16 +1,35 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 using Serilog;
 
 namespace CarvedRock.InvoiceGenerator
 {
     class Program
     {
+        private static IConfiguration _config;
+
         static void Main(string[] args)
         {
+            _config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory)?.FullName)
+                .AddJsonFile("appsettings.json", false)
+                .AddEnvironmentVariables()
+                .Build();
+
             ConfigureLogging();
 
             try
             {
+                var connectionString = _config.GetConnectionString("Db");
+                var simpleProperty = _config.GetValue<string>("SimpleProperty");
+                var nestedProp = _config.GetValue<string>("Inventory:NestedProperty");
+
+                Log.ForContext("ConnectionString", connectionString)
+                    .ForContext("SimpleProperty", simpleProperty)
+                    .ForContext("Inventory:NestedProperty", nestedProp)
+                    .Information("Load configuration", connectionString);
+
                 Log.ForContext("Args", args)
                    .Information("Starting program...");
 
